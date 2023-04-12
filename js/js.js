9d004b6d10047cc0de8b2058f0728daade02f263
SitingXing <121953533+SitingXing@users.noreply.github.com>
2023-04-12 13:41:06 -0400

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,5 +1,14 @@
 const menuBtn = document.querySelector(".menu-btn");
 const navigation = document.querySelector(".navigation");
+const steamBtn = document.querySelector(".steamBtn");
+const wikiBtn = document.querySelector(".wikiBtn");
+const developerBtn = document.querySelector(".developerBtn");
+
+const steamLinks = ['https://store.steampowered.com/app/1443430/Rogue/', 'https://store.steampowered.com/app/250900/The_Binding_of_Isaac_Rebirth/', 'https://store.steampowered.com/app/646570/Slay_the_Spire/', 'https://store.steampowered.com/app/588650/Dead_Cells/'];
+
+const wikiLinks = ['https://en.wikipedia.org/wiki/Rogue_(video_game)', 'https://bindingofisaacrebirth.fandom.com/wiki/Binding_of_Isaac:_Rebirth_Wiki', 'https://slay-the-spire.fandom.com/wiki/Slay_the_Spire_Wiki', 'https://deadcells.fandom.com/wiki/Dead_Cells_Wiki'];
+
+const developerLinks = ['https://en.wikipedia.org/wiki/Glenn_Wichman', 'https://en.wikipedia.org/wiki/Edmund_McMillen', 'https://en.wikipedia.org/wiki/Humble_Bundle', 'https://en.wikipedia.org/wiki/Motion_Twin']
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active");
@@ -32,6 +41,9 @@ const sliderNav = (manual) => {
     slides[manual].classList.add("active");
     contents[manual].classList.add("active");
     icons[manual].classList.add("active");
+    steamBtn.setAttribute('href', steamLinks[manual]);
+    wikiBtn.setAttribute('href', wikiLinks[manual]);
+    developerBtn.setAttribute('href', developerLinks[manual]);
 }
 
 btns.forEach((btn, i) => {
--- js/js2.js
+++ js/js2.js
@@ -4,9 +4,18 @@ const header = document.querySelector("header");
 const shadow = document.querySelector(".shadow");
 const description = document.querySelectorAll(".descriptionContainer");
 const gameDescription = document.querySelector(".gameDescription");
+const characters = document.querySelectorAll(".character");
+const characterContainer = document.querySelector(".characteristics");
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
 
 let headerHeight = header.offsetHeight;
 let gameDescriptionHeight = gameDescription.offsetHeight;
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
 
 window.addEventListener('scroll', ()=> {
     let scroll = window.pageYOffset;
@@ -28,6 +37,47 @@ window.addEventListener('scroll', ()=> {
         };
     });
 
+    characters.forEach(element => {
+        const elementTop = element.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (elementTop < windowHeight * 2 / 3){
+            element.style.opacity = 1;
+        } else {
+            element.style.opacity = 0;
+        };
+    });
+
     bigTitle.style.opacity = -scroll / (headerHeight / 2) + 1;
     shadow.style.height = `${scroll * 0.5 + 600}px`
 })
+
+const apiKey = '6d8a9e88b0104ad0bfe2f2f578e4bc7e';
+let gameName = ''
+if (bigTitle.textContent === 'Binding of Issac:Rebirth') {
+    gameName = 'the-binding-of-isaac-rebirth';
+} else if (bigTitle.textContent === 'Slay the Spire') {
+    gameName = 'slay-the-spire';
+} else if (bigTitle.textContent === 'Dead Cells') {
+    gameName = 'dead-cells'
+}
+
+if (gameName !== ''){
+    const url = `https://api.rawg.io/api/games/${gameName}?key=${apiKey}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector(".rating").innerHTML += ` ${data.rating}`;
+        })
+
+    const achievementUrl = `https://api.rawg.io/api/games/${gameName}/achievements?key=${apiKey}`
+
+    fetch(achievementUrl)
+        .then(response => response.json())
+        .then(data => {
+            document.querySelector(".achievements").innerHTML += `<p class="text">Total ${data.count}`;
+            const images = data.results.map(result => `<div><img src="${result.image}" alt=${result.name}></div>`);
+            document.querySelector(".achievements").innerHTML += `<div class="achieveImages">${images.join("")}</div>`;
+        })
+}
